53b495720f79ca6317f6aea74259f7f4fc1f5eb3
BelOlego <tristan2ster@gmail.com>
2015-04-02 19:18:30 +0200

    Changed test string

--- auto_copy.bat
+++ auto_copy.bat
@@ -1,4 +1,3 @@
-::Test editing
 copy C:\Users\o_belyanski\Desktop\*.doc D:\TZ\Archive\
 	if exist \\AL\_tz_qa_2015\2015\tz_QA_game_backup.doc GOTO Last 
 move C:\Users\o_belyanski\Desktop\*.doc D:\TZ\Upload_TZ\
